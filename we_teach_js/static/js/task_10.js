b72a140ae604805058d1afdaec3c135587eac76b
Viktor <vm-viktor@mail.ru>
2023-12-29 09:49:44 +0700

    update readme file

--- we_teach_js/static/js/task_10.js
+++ we_teach_js/static/js/task_10.js
@@ -5,15 +5,63 @@ function result () {
     if (+year == full_year) {
         document.getElementById("primer").innerHTML = 
         `<p>let year = prompt('Какой сегодня год? ', "")</p><p>let data = new Date()</p>
-        <p>let full_year = data.getFullYear()</p><p>if (+year == full_year) {</p><p style="padding-left: 20px;">'И это правильно!'</p><p>}else {</p>
-        <p style="padding-left: 20px;">alert("Советую еще подумать!")</p><p>}</p>
+        <p>let full_year = data.getFullYear()</p><p>if (+year == full_year) {</p><p style="padding-left: 20px;">'И это правильно!'</p><p>}
         <button type="submit" onclick="remove()">Стереть</button>`
+        alert("И это правильный ответ!")
+    }// else {
+    //     alert("Советую еще подумать!")
+    // }
+}
+
+function blockElse () {
+    let year = prompt('Какой сегодня год? ', "")
+    let data = new Date()
+    let full_year = data.getFullYear()
+    if (+year == full_year) {
         alert("И это правильный ответ!")
     } else {
         alert("Советую еще подумать!")
     }
 }
 
+function blockElseIf () {
+    let year = prompt('В каком году была опубликована спецификация ECMAScript-2015?', '');
+    if (year < 2015) {
+        alert( 'Это слишком рано...' );
+    } else if (year > 2015) {
+        alert( 'Это поздновато' );
+    } else {
+        alert( 'Верно!' );
+    }
+}
+
+function ternaryOperaror () {
+    let age = +prompt('Сколько вам лет?', '')
+    let accessAllowed = (age > 18) ? true : false;
+
+    (accessAllowed) ? 
+        document.getElementById("age").innerHTML = `<p style="color: green;">Вы старше 18 лет. Вам ${age} лет. Доступ открыт</p>`:
+        document.getElementById("age").innerHTML = `<p style="color: red;"> Вы младше 18 лет. Вам ${age} лет. Доступ закрыт</p>`;
+
+    alert(`Значение переменной 'accessAllowed' - ${accessAllowed}`)
+}
+
 function remove (){
     document.getElementById('primer').innerHTML = ""
 }
+
+function popupWindow (url, title, width, height) {
+    let pos_left = window.screenLeft ? window.screenLeft : window.screenX;
+    let pos_top = window.screenTop ? window.screenTop : window.screenY;
+    console.log(`pos_left - ${pos_left}; pos_top - ${pos_top}`)
+    let left = pos_left + (window.innerWidth / 2) - (width / 2);
+    let top = pos_top + (window.innerHeight / 2) - (height / 2);
+    console.log(`left - ${left}; top - ${top}`)
+    
+    return window.open(url, title, 'toolbar=no, location=no, directories=no, status=yes, menubar=no,scrollbars=yes, resizable=no, copyhistory=no, width=' + width + ', height=' + height + ', top=' + top + ', left=' + left);
+}
+// function popupWindow(url, title, w, h) {
+//     var left = (screen.width/2)-(w/2);
+//     var top = (screen.height/2)-(h/2);
+//     return window.open(url, title, 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width='+w+', height='+h+', top='+top+', left='+left);
+//   } 
