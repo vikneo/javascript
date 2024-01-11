bd29f2041129a839de2612421365d6f038f6d66c
Viktor <vm-viktor@mail.ru>
2024-01-11 14:05:06 +0700

    updated task_2

---
+++ we_teach_js/static/js/part_2/task_2.js
@@ -0,0 +1,6 @@
+function viewChild () {
+    // alert(document.body.childNodes)
+    for (let i = 0; i < document.body.childNodes.length; i++) {
+        alert('' + document.body.childNodes[i])
+    }
+}
