1296061f68d342feaf5bffae3635ccf0b6edfcb0
Viktor Martynov <vm-viktor@mail.ru>
2024-05-20 22:35:30 +0700

    update file handle.js

--- javascript/handler_event/handler.js
+++ javascript/handler_event/handler.js
@@ -1,16 +1,29 @@
 function handleClick () {
-    console.log('Clicked!')
+    console.log(`Clicked!`)
+}
+
+function handleClickRemove () {
+    alert(`С кнопки "click 2" - будет снято событие`)
+}
+
+function handleClickEvent (event) {
+    // Вывод текстовое содержимое каждой кнопки "event.target.textContent"
+    console.log(`Clicked! event - ${event.target.textContent}`)
+    // вывод свойства "data"
+    console.log(event.target.dataset)
+    console.log(`Строка (свойства "data") - ${event.target.dataset.num}`)
+    console.log(`Число (свойства "data") - ${parseInt(event.target.dataset.num)}`)
 }
 
 function Button () {
-    let button = document.querySelector('.btn1')
-    let button2 = document.querySelector('.btn2')
+    let button = document.querySelector('.btn1');
+    let button2 = document.querySelector('.btn2');
     
     button.addEventListener('click', handleClick)
-    button2.addEventListener('click', handleClick)
+    button2.addEventListener('click', handleClickRemove)
 
     // Снять обработчик с кнопки
-    button2.removeEventListener('click', handleClick)
+    button2.removeEventListener('click', handleClickRemove)
 }
 
 function Buttons () {
@@ -18,6 +31,16 @@ function Buttons () {
 
     // Получить доступ к каждой кнопки через метод "forEach"
     buttons.forEach(button => {
-        button.addEventListener('click', handleClick)
+        button.addEventListener('click', handleClick);
     });
+}
+
+// ==== Всплытие и погружение (Фазы Жизненного Цикла События) ===
+
+function buttonEvent () {
+    let buttons = document.querySelectorAll('.btn');
+
+    buttons.forEach(button => {
+        button.addEventListener('click', handleClickEvent)
+    })
 }
