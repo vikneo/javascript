49a5f7977d04361681b5b22fa42c6b843b27c082
Viktor Martynov <vm-viktor@mail.ru>
2024-05-24 23:48:28 +0700

    created tag "h1"

---
+++ javascript/static/js/object_set.js
@@ -0,0 +1,9 @@
+// Создать тег h1
+const h1 = document.createElement('h1')
+// добавить текст "Объекты и масивы. Как правильно копировать!?"
+h1.innerText = "Объекты и масивы. Как правильно копировать!?"
+// Вставить его в тег body
+const body = document.body
+body.appendChild(h1);
+
+
