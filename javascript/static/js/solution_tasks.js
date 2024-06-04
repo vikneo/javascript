50262beb5bb0e9692b7cb1f1483d506355f07a42
Viktor Martynov <vm-viktor@mail.ru>
2024-06-04 21:56:34 +0700

    added task_3

--- javascript/static/js/solution_tasks.js
+++ javascript/static/js/solution_tasks.js
@@ -34,3 +34,29 @@ const setFruit = (list) => {
 
 console.log(setFruit(myfruits))
 
+// ================ TASK 3 =========================
+
+// 3. Создать функцию, которая сгрупирует студентов по возрасту.
+// На выходе получить объект, где ключ - возраст,
+// а значение - массив студентов
+
+const students =[
+    {name: 'Alex', age: 25},
+    {name: 'Mike>', age: 28},
+    {name: 'Masha', age: 25},
+    {name: 'Stas', age: 18},
+];
+
+const groupStudents = (list) => {
+    const gruped = {};
+    list.forEach(student => {
+        if (!gruped[student.age]) {
+            gruped[student.age] = [student];
+        } else {
+            gruped[student.age].push(student);   
+        }
+    });
+    return gruped;
+}
+
+console.log(groupStudents(students))
