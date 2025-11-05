0ebda5aeab84d37366871d6762577f4cfb29728f
vikneo <vm-viktor@mail.ru>
2025-11-05 21:18:35 +0700

    added verification of the entered character in the word

--- gallows/static/js/main.js
+++ gallows/static/js/main.js
@@ -1,18 +1,49 @@
 const setWords = ['полнеба', 'потреба', 'ворожба', 'служба', 'метеослужба', 'дружба', 'диатриба', 'планшайба', 'надолба', 'катакомба', 'пломба', 'гекатомба', 'клумба', 'блямба', 'жадоба', 'худоба', 'стыдоба', 'жалоба', 'зазноба', 'хвороба', 'утроба', 'чащоба', 'чищоба', 'трущоба', 'пагуба', 'палуба', 'голуба', 'йоруба', 'стереотруба', 'волшба', 'свадьба', 'усадьба', 'городьба', 'ходьба', 'судьба', 'резьба', 'похвальба', 'пальба', 'стрельба', 'мольба', 'гульба', 'гоньба', 'бороньба', 'борьба', 'гурьба', 'косьба', 'просьба', 'татьба', 'селитьба', 'женитьба', 'молотьба', 'пастьба', 'голытьба', 'забава', 'держава', 'шалава', 'облава', 'булава']
 
-let random_word = setWords[Math.ceil(Math.random() * setWords.length)]
-let word = random_word.split("")
+let random_word = setWords[Math.ceil(Math.random() * setWords.length)];
+// let random_word = "порошок"; // for debug
+let word = random_word.split("");
 
-console.log(word)
-
-let grid = document.querySelector(".grid")
+let grid = document.querySelector(".grid");
 grid.style.gridTemplateColumns = `repeat(${word.length}, 1fr)`;
 grid.style.color = `blue`;
 
 for (let i = 0; i < word.length; i++) {
-    let div = document.createElement("div")
-    div.className = 'element'
-    div.innerHTML = "&ast;" //word[i]
-    grid.appendChild(div)
+    let div = document.createElement("div");
+    div.id = i + 1;
+    div.className = 'element';
+    // div.innerHTML = "&#x1F637;" //word[i]
+    grid.appendChild(div);
 }
-console.log(grid)
+
+document.querySelector(".input").addEventListener("input", function(e) {
+    let flag = false;
+    let sym = e.target.value;
+    let elems = document.querySelectorAll(".element");
+
+    if (sym === "") return;
+    
+    for (let i = 0; i < word.length; i++) {
+        if (word[i].toLowerCase() === sym.toLowerCase()) {
+
+            for (let j of elems) {
+                if (+j.id == i + 1) {
+                    j.innerHTML = word[i].toUpperCase();
+                    j.className = "element back";
+                    j.style.transform = "rotateX(360deg)";
+                    j.style.color = "black";
+                    j.style.backgroundColor = "white";
+                    flag = true
+                }
+            }
+        }
+    }
+    let _id = setInterval(() => {
+        e.target.value = ''
+    }, 1000);
+
+    if (!flag) {
+        console.log("Рисуем виселицу")
+        // showGallows();
+    }
+})
