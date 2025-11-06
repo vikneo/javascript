069379a90d78d432b2e0bc5bcfcdfbf40b132f58
vikneo <vm-viktor@mail.ru>
2025-11-06 20:37:44 +0700

    added a field for entered letters

--- gallows/static/files/read_file.py
+++ gallows/static/files/read_file.py
@@ -13,22 +13,23 @@ file_name = 'russian_nouns.txt'
 write_file_name = 'words.txt'
 
 path_to_read_file = os.path.join(file_path, file_name)
-path_to_write_file = os.path.join(file_path, write_file_name)
+path_to_write_file = Path(os.path.join(file_path, write_file_name))
 
-def sorting_words():
+def write_to_file(file_name, method):
     try:
-        with open(path_to_read_file, 'r', encoding="utf8") as file:
-            for f in file.readlines():
-                elem = f.lstrip().strip()
-                if 7 < len(f) < 9  and "-" not in elem:
-                    with open(path_to_write_file, 'a', encoding="utf8") as write_file:
-                        write_file.write(f"{f}")
+        if not file_name.exists():
+            with open(path_to_read_file, 'r', encoding="utf8") as file:
+                for f in file.readlines():
+                    if 8 <= len(f) < 9  and "-" not in f:
+                        with open(file_name, method, encoding="utf8") as write_file:
+                            write_file.write(f"{f}")
+        else:
+            raise FileExistsError("Файл уже существует")
     except Exception as err:
         print(err)
 
 
-def write_to_file():
-    sorting_words()
+def sorting_words():
     words_list = []
 
     try:
@@ -43,7 +44,7 @@ def write_to_file():
 
 def action():
     random_list = []
-    words_list = write_to_file()
+    words_list = sorting_words()
     
     for _ in range(50):
         random_list.append(choice(words_list))
@@ -52,5 +53,8 @@ def action():
 
 
 if __name__ == "__main__":
-    
-    print(action())
+    try:
+        write_to_file(path_to_write_file, "a")
+        print(action())
+    except Exception as err:
+        print(err)
--- gallows/static/js/main.js
+++ gallows/static/js/main.js
@@ -1,38 +1,68 @@
-const setWords = ['полнеба', 'потреба', 'ворожба', 'служба', 'метеослужба', 'дружба', 'диатриба', 'планшайба', 'надолба', 'катакомба', 'пломба', 'гекатомба', 'клумба', 'блямба', 'жадоба', 'худоба', 'стыдоба', 'жалоба', 'зазноба', 'хвороба', 'утроба', 'чащоба', 'чищоба', 'трущоба', 'пагуба', 'палуба', 'голуба', 'йоруба', 'стереотруба', 'волшба', 'свадьба', 'усадьба', 'городьба', 'ходьба', 'судьба', 'резьба', 'похвальба', 'пальба', 'стрельба', 'мольба', 'гульба', 'гоньба', 'бороньба', 'борьба', 'гурьба', 'косьба', 'просьба', 'татьба', 'селитьба', 'женитьба', 'молотьба', 'пастьба', 'голытьба', 'забава', 'держава', 'шалава', 'облава', 'булава']
+const setWords = JSON.parse(localStorage.getItem("setWords"))
+const alphabet = JSON.parse(localStorage.getItem("alphabet"))
 
 let random_word = setWords[Math.ceil(Math.random() * setWords.length)];
 // let random_word = "порошок"; // for debug
 let word = random_word.split("");
 
-let grid = document.querySelector(".grid");
-grid.style.gridTemplateColumns = `repeat(${word.length}, 1fr)`;
-grid.style.color = `blue`;
+function generateWord() {
 
-for (let i = 0; i < word.length; i++) {
-    let div = document.createElement("div");
-    div.id = i + 1;
-    div.className = 'element';
-    div.innerHTML = "&#x1F637;" //word[i]
-    grid.appendChild(div);
+    let grid = document.querySelector(".word-block");
+    grid.style.gridTemplateColumns = `repeat(${word.length}, 1fr)`;
+    grid.style.color = `blue`;
+    
+    for (let i = 0; i < word.length; i++) {
+        let div = document.createElement("div");
+        div.id = i + 1;
+        div.className = 'element';
+        div.innerHTML = "&#x1F637;" //word[i]
+        grid.appendChild(div);
+    }
+}
+
+function createdFieldAlphabet() {
+    let alpField = document.querySelector(".alphabet-field");
+    alpField.style.gridTemplate = `repeat(3, 14px) / repeat(${alphabet.length / 3}, 13.8px)`;
+    alpField.style.rowGap = "10px"
+
+    for(let i = 0; i < alphabet.length; i++) {
+        let div = document.createElement("div");
+
+        div.id = i + 1;
+        div.className = "alphabet";
+        div.innerHTML = alphabet[i]
+        div.style = "display: none"
+        alpField.appendChild(div)
+
+    }
 }
 
 document.querySelector(".input").addEventListener("input", function(e) {
     let flag = false;
     let sym = e.target.value;
     let elems = document.querySelectorAll(".element");
+    let fieldAlpha = document.querySelectorAll(".alphabet")
 
     if (sym === "") return;
     
+    fieldAlpha.forEach((symbol) => {
+        if (symbol.innerHTML === sym.toUpperCase()) {
+            symbol.style.display = "block";
+            symbol.style.backgroundColor = 'grey';
+            symbol.style.color = 'white';
+        }
+    })
+
     for (let i = 0; i < word.length; i++) {
         if (word[i].toLowerCase() === sym.toLowerCase()) {
 
-            for (let j of elems) {
-                if (+j.id == i + 1) {
-                    j.innerHTML = word[i].toUpperCase();
-                    j.className = "element back";
-                    j.style.transform = "rotateX(360deg)";
-                    j.style.color = "black";
-                    j.style.backgroundColor = "white";
+            for (let elem of elems) {
+                if (+elem.id == i + 1) {
+                    elem.innerHTML = word[i].toUpperCase();
+                    elem.className = "element back";
+                    elem.style.transform = "rotateX(360deg)";
+                    elem.style.color = "black";
+                    elem.style.backgroundColor = "white";
                     flag = true
                 }
             }
@@ -40,10 +70,15 @@ document.querySelector(".input").addEventListener("input", function(e) {
     }
     let _id = setInterval(() => {
         e.target.value = ''
-    }, 1000);
+    }, 3000);
 
     if (!flag) {
         console.log("Рисуем виселицу")
         // showGallows();
     }
 })
+
+generateWord()
+createdFieldAlphabet()
+
+
