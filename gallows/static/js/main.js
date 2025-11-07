37d29f2d36a630f92ba401fb90388ae2d389d5a0
Viktor <vm-viktor@mail.ru>
2025-11-07 16:49:17 +0700

    added the drawing of the gallows

--- gallows/static/js/main.js
+++ gallows/static/js/main.js
@@ -1,5 +1,8 @@
 const setWords = JSON.parse(localStorage.getItem("setWords"))
-const alphabet = JSON.parse(localStorage.getItem("alphabet"))
+const alphabetObj = JSON.parse(localStorage.getItem("alphabet"))
+const alphabet = alphabetObj['alphabet']
+// let count = alphabetObj['count']
+let count = 0;
 
 let random_word = setWords[Math.ceil(Math.random() * setWords.length)];
 // let random_word = "порошок"; // for debug
@@ -68,17 +71,76 @@ document.querySelector(".input").addEventListener("input", function(e) {
             }
         }
     }
-    let _id = setInterval(() => {
+    setInterval(() => {
         e.target.value = ''
-    }, 3000);
+    }, 1000);
 
     if (!flag) {
+        count++;
         console.log("Рисуем виселицу")
-        // showGallows();
+        showGallows(count);
+        localStorage.setItem("alphabet", JSON.stringify({"alphabet": alphabet, "count": count}))
     }
 })
 
+let stylus = caffold()
+
+function showGallows(pos) {
+    // Рисуем виселицу из не правильно отгаданных букв
+    console.log(count)
+    switch (pos) {
+        case 1: 
+        // рисуем левый столб
+            stylus.strokeRect(100, 40, 2, 110);
+            break;
+        case 2: 
+        // рисуем перекладину
+            stylus.strokeRect(100, 40, 70, 2);
+            break;
+        case 3: 
+        // рисуем петлю
+            stylus.strokeRect(170, 40, 2, 20);
+            break;
+        case 4: 
+        // рисуем голову
+            stylus.arc(170, 70, 7, 0, Math.PI * 2, true);
+            stylus.fill();
+            break;
+        case 5: 
+        // рисуем туловище
+            stylus.beginPath();
+            stylus.moveTo(170, 78);
+            stylus.quadraticCurveTo(185, 100, 170, 110);
+            stylus.quadraticCurveTo(155, 100, 170, 78);
+            stylus.fill();
+            break;
+        case 6: 
+        // рисуем руки
+        // левая рука
+            stylus.beginPath();
+            stylus.moveTo(168, 78);
+            stylus.quadraticCurveTo(160, 88, 152, 108);
+            stylus.quadraticCurveTo(173, 86, 168, 78);
+            stylus.fill();
+        // правая рука
+            stylus.beginPath();
+            stylus.moveTo(172, 78);
+            stylus.quadraticCurveTo(180, 86, 188, 108);
+            stylus.quadraticCurveTo(174, 90, 172, 78);
+            stylus.fill();
+            break;
+        case 7: 
+        // рисуем ноги
+            // stylus.arc(170, 70, 7, 0, Math.PI * 2, true);
+            // stylus.fill();
+            break;
+        case 8: 
+        // рисуем стул
+            // stylus.arc(170, 70, 7, 0, Math.PI * 2, true);
+            // stylus.fill();
+            break;
+    }
+}
+
 generateWord()
 createdFieldAlphabet()
-
-
