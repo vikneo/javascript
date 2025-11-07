c5e4d1e4ca2b622b584efb9d1195fb524b289a09
Viktor <vm-viktor@mail.ru>
2025-11-07 16:46:43 +0700

    ded the display of the scaffold

--- gallows/static/js/constreem.js
+++ gallows/static/js/constreem.js
@@ -11,10 +11,47 @@ const alphabetKir = [
 ]
 
 if (!localStorage.getItem("setWords")) {
-    localStorage.setItem("setWords", JSON.stringify(setWords))
+    localStorage.setItem("setWords", JSON.stringify(setingWords))
 }
 
 if (!localStorage.getItem("alphabet")) {
-    localStorage.setItem("alphabet", JSON.stringify(alphabet))
+    localStorage.setItem("alphabet", JSON.stringify({"alphabet": alphabetKir, "count": 0}))
 }
 
+
+function caffold() {
+    // Рисуем площадку для игрока
+
+    const canvas = document.getElementById("gallows_field")
+
+    if (canvas.getContext) {
+        const ctx = canvas.getContext("2d")
+        var gridSize = 10;  
+        
+        // // Отрисовка горизонтальных линий  
+        // for (var x = 0; x < canvas.width; x += gridSize) {  
+        //     ctx.moveTo(x, 0);  
+        //     ctx.lineTo(x, canvas.height);  
+        // }  
+        // // Отрисовка вертикальных линий  
+        // for (var y = 0; y <= canvas.height; y += gridSize) {  
+        //     ctx.moveTo(0, y);  
+        //     ctx.lineTo(canvas.width, y);
+        // }
+        // ctx.strokeStyle = '#414141ff';
+        // ctx.strokeStyle = '#f3f2f2ff';
+        // ctx.stroke();
+
+        ctx.beginPath();
+        ctx.moveTo(50, 150);
+        ctx.strokeRect(80, 150, 150, 5);
+        ctx.strokeRect(50, 155, 180, 5);
+        // ctx.lineWidth(5);
+        ctx.strokeStyle = '#7a7373ff';
+
+        return ctx
+
+    } else {
+        alert("Ваш браузер не поддерживает Canvas")
+    }
+}
