3aa59f1b5721b72abcd3c849a43aea4bd6f2c1db
vikneo <vm-viktor@mail.ru>
2025-11-13 19:08:10 +0700

    created game the Sudoku

---
+++ sudoku/static/js/main.js
@@ -0,0 +1,29 @@
+let factArrayNumber = [];
+
+function generateArrayNumber() {
+    for (let i = 0; i < 9; i++) {
+        let rowNumber = [];
+        for (let num = 1; num <= 9; num++) {
+            rowNumber.push(num);
+        }
+
+        factArrayNumber.push(rowNumber);
+    }
+}
+
+function createdFielGame() {
+    let box = document.getElementById("sudoku")
+
+    for (row = 0; row < factArrayNumber.length; row++) {
+        for (let i = 0; i < factArrayNumber[row].length; i++) {
+            let div = document.createElement("div")
+            div.className = "sudoku_field";
+            div.innerHTML = factArrayNumber[row][i];
+            box.appendChild(div);
+        }
+    }
+}
+
+generateArrayNumber()
+createdFielGame()
+console.log(factArrayNumber);
